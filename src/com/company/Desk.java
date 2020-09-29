0b68e218b3fd07448f142ec5faa423bded7e6865
tsragravorogh <71145107+tsragravorogh@users.noreply.github.com>
2020-09-29 08:54:15 +0300

    Commit 8:55 29.09

--- src/com/company/Desk.java
+++ src/com/company/Desk.java
@@ -2,24 +2,62 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
-public class Desk {
-    private Stack<Card> cards;
+public enum  Desk {
 
-    public Desk() {
-        cards = new Stack<Card>();
+    INSTANCE;
 
-        ArrayList<Card> allCards = new ArrayList<Card>();
-        for (String rank : DescriptionCards.ranks) {
-            for (String suit : DescriptionCards.suits) {
-                allCards.add(new Card(rank, suit));
-                Collections.shuffle(allCards);
+    private List<Card> cards;
+    private List<Card> playDeck;
+
+    Desk() {
+        cards = new ArrayList<>(52);
+        for (Suit suit : Suit.items) {
+            for (Face face : Face.items) {
+                cards.add(new Card(suit, face));
             }
         }
-
-        for (Card card : allCards) {
-            cards.push(card);
-        }
+        playDeck = new ArrayList<>(cards);
     }
+
+    public void shuffle() {
+        playDeck.clear();
+        playDeck.addAll(cards);
+        Collections.shuffle(playDeck);
+    }
+
+    public Card pop() {
+        if (playDeck.isEmpty()) {
+            return null;
+        }
+        return playDeck.remove(0);
+    }
+
+    public void push(Card card) {
+        playDeck.add(card);
+    }
+
+
+
+
+
+//    private Stack<Card> cards;
+//
+//    public Desk() {
+//        cards = new Stack<Card>();
+//
+//        ArrayList<Card> allCards = new ArrayList<Card>();
+//        for (String rank : DescriptionCards.ranks) {
+//            for (String suit : DescriptionCards.suits) {
+//                allCards.add(new Card(rank, suit));
+//                Collections.shuffle(allCards);
+//            }
+//        }
+//
+//        for (Card card : allCards) {
+//            cards.push(card);
+//        }
+//    }
 }
